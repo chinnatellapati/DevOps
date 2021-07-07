d6fc4bda7cd08f8dd0463a1dc81a9cb4af4c3b00
chinnabits <2020mt93203@wilp.bits-pilani.ac.in>
2021-07-07 22:18:34 +0530

    addition of two numbers in python

---
+++ Network-Automation/add2num.py
@@ -0,0 +1,20 @@
+# Addition of two numbers in python
+
+num1 = int(input("Enter num1\n"))
+num2 = int(input("Enter num2\n"))
+sum = int(num1+num2)
+print('sum of two numbers', sum)
+
+
+
+# Addition of two numbers in python
+
+num1 = int(input("Enter num1\n"))
+num2 = int(input("Enter num2\n"))
+sum = int(num1+num2)
+print(num1, " + ", num2, " = ", sum)
+
+
+
+name = input('what is your name? ')
+print('Hi ' + name)
