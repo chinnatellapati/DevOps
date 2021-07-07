d022cbf7409eb8055ee0fd660f7995a65329b87e
chinnabits <2020mt93203@wilp.bits-pilani.ac.in>
2021-07-08 00:54:41 +0530

    ifelse in python

---
+++ Network-Automation/ifelse.py
@@ -0,0 +1,14 @@
+# If else statements in Python
+
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It's cold day")
+    print("Wear warm clothes")
+else:
+    print("It's lovely day")
+print("Enjoy your day")
