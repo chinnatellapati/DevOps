1eb5add9b4d09ccfa5b4cb24f64f82e3a25ddaf3
chinnabits <2020mt93203@wilp.bits-pilani.ac.in>
2021-07-08 23:32:58 +0530

    2 Dimentional lists

---
+++ Network-Automation/2dlist.py
@@ -0,0 +1,13 @@
+# 2 Dimentinal lists in Python .
+
+matrix =[
+
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ]
+print(f' Print first_row second element: {matrix[0][1]}')
+for row in matrix:
+    for item in row:
+        print(item)
+
