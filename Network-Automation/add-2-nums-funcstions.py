a72e8960eae1a4f59147a215309c4f78352a8bdc
chinnabits <2020mt93203@wilp.bits-pilani.ac.in>
2021-07-09 15:50:11 +0530

    adding two numbers reading input from keyboard

---
+++ Network-Automation/add-2-nums-funcstions.py
@@ -0,0 +1,10 @@
+import math
+def addition_of_numers():
+    value1 = int(input('value1 '))
+    value2 = int(input('value2 '))
+    c = value1 + value2
+    return c
+
+
+sum = addition_of_numers()
+print(f'sum of two numbers=', sum)
