ee8ad0af2317a489795eb7722f22c54a8c216633
chinnabits <2020mt93203@wilp.bits-pilani.ac.in>
2021-07-09 16:57:57 +0530

    classes in python

---
+++ Network-Automation/class_In_Python.py
@@ -0,0 +1,25 @@
+# Classes in Python
+# classes will be used to create new data types such as shopping cart...etc. Shopping cart will not a list, dictionary, tuple, boolean , string,array...et
+
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+point1 = Point() # object
+
+point1.move()
+point1.x = 20
+print(point1.x)
+point1.y=30
+point1.draw()
+
+
+point2 = Point()
+point2.x = 10
+print(point2.x)
+
+
+
