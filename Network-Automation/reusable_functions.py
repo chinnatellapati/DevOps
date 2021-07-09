bc189d10198989e3aa619ed15c047239ed7f8107
chinnabits <2020mt93203@wilp.bits-pilani.ac.in>
2021-07-09 16:02:55 +0530

    Code for reusable functions added

---
+++ Network-Automation/reusable_functions.py
@@ -0,0 +1,19 @@
+# Re-usable python program
+
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {":)": "😊",
+              ":(": "😒"
+              }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+message = input('> ')
+result = emoji_converter(message)
+print(result)
+
+# print(emoji_converter(message))
+
